c2c35f38c3e98ebb67ef67b2b16d2524b84b3167
RoseniaViolet <150266058+RoseniaViolet@users.noreply.github.com>
2023-11-11 01:05:11 +0700

    Trangcslt2-57.py

--- Exercise35.py
+++ Exercise35.py
@@ -1,6 +1,6 @@
 human_age = int(input('Input a dog age in human years:'))
 if human_age < 0:
-    print('The age of the dog must be a positive number')
+    print('The age of the dog must be a positive number.')
 elif human_age <= 2:
     dog_age = human_age * 10.5
 else:
